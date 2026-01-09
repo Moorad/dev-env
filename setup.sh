632de902e1ddfefe9f04d849fc37a5fb41505cda
Moorad Altamimi <maltamimi@alibabacloud.sa>
2026-01-10 00:13:24 +0300

    add tmux, vim, ghostty and lazygit to setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+function announce() {
+	message=$(printf "$1")
+	gum style --border="normal" --align="center" --padding="1 2" "$message"
+}
+
 function backup() {
 	FILE_PATH=$1
 	FILE=$(basename "$FILE_PATH")
@@ -17,23 +22,35 @@ function replace() {
 	echo "Replaced $(basename $FILE_2)"
 }
 
-if ! brew bundle check --verbose --no-upgrade; then
-	brew bundle install --no-upgrade
-fi
+function check_replace() {
+	dev_env_location=$1
+	original_location=$2
+	file=$(basename $(dirname $dev_env_location))
 
-echo "Checking .zshrc"
-if ! git --no-pager diff --no-index  ~/.zshrc ./.zshrc; then
-	if gum confirm "Do you want to replace your .zshrc anyways"; then
-		backup ~/.zshrc
-		replace ./.zshrc ~/.zshrc 
-		gum style --border="normal" --align="center" --padding="1 2" "$(cat <<'EOF'
-Reload your zshrc to apply the changes to the current shell:
-
-source ~/.zshrc
-EOF
-)"
+	echo ""
+	echo "Checking $file config"
+	if ! git diff --no-index $original_location $dev_env_location; then
+		if gum confirm "Do you want to replace your $file anyways"; then
+			backup $original_location
+			replace $dev_env_location $original_location
+		fi
+	else
+		echo "No difference in $file config"
+		return 1
 	fi
-else 
-	echo "No difference in .zshrc"
+}
+
+echo "Checking brew formulae and casks"
+if ! (cd ./config/brew && brew bundle check --verbose --no-upgrade); then
+	(cd ./config/brew && brew bundle install --no-upgrade)
 fi
 
+check_replace ./config/zsh/.zshrc ~/.zshrc && announce "Reload your zshrc to apply the changes to the current shell:\n\nsource ~/.zshrc"
+
+check_replace ./config/tmux/.tmux.conf ~/.tmux.conf && announce "Reload your tmux config to apply the changes to the current session:\n\ntmux source-file ~/.tmux.conf"
+
+check_replace ./config/vim/.vimrc ~/.vimrc || true
+
+check_replace ./config/ghostty/config ~/.config/ghostty/config && announce "Reload your ghostty config to apply the changes:\n\n⌘ + SHIFT + ,"
+
+check_replace ./config/lazygit/config.yml ~/.config/lazygit/config.yml || true
