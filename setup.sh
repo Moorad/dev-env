1e456b5c70f01b53d3d1daa87454c945b6ef9ad5
Moorad <moorad2003@gmail.com>
2026-01-17 17:33:35 +0300

    Pretty printing and script syncing

--- setup.sh
+++ setup.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+GREEN=2
+YELLOW=3
+BLUE=4
+PURPLE=5
+CYAN=6
+
+function pretty_print() {
+	gum style --foreground $1 "$2"
+}
+
 function announce() {
 	message=$(printf "$1")
 	gum style --border="normal" --align="center" --padding="1 2" "$message"
@@ -12,44 +22,55 @@ function backup() {
 	BACKUP_LOCATION=/var/tmp/$FILE.backup
 
 	sudo cp $FILE_PATH $BACKUP_LOCATION
-	echo "Backed up $FILE to $BACKUP_LOCATION"
+	pretty_print $CYAN "Backed up $FILE to $BACKUP_LOCATION"
 }
 
 function replace() {
 	FILE_1=$1
 	FILE_2=$2
 	cp $FILE_1 $FILE_2
-	echo "Replaced $(basename $FILE_2)"
+	pretty_print $GREEN "Replaced $(basename $FILE_2)"
 }
 
 function check_replace() {
 	dev_env_location=$1
 	original_location=$2
-	file=$(basename $(dirname $dev_env_location))
+	file="$(basename $(dirname $dev_env_location)) config"
+	color=$YELLOW
 
-	echo ""
-	echo "Checking $file config"
-
-	if [ ! -f $original_location ]; then 
-		echo "It seems like the $file config does not exist in your system, creating $file config"
-	 	mkdir -p $(dirname $original_location)
-		touch $original_location
+	if [[ $file == "bin config" ]]; then
+		file="$(basename $dev_env_location) script"
+		color=$PURPLE
 	fi
 
-	has_diff=$(cmp -s $original_location $dev_env_location; echo $?)  
+	pretty_print $color "Checking $file"
 
-	if  [[ $has_diff -eq 1 ]] && ! git diff --no-index $original_location $dev_env_location; then
+	if [ ! -f $original_location ]; then
+		pretty_print $CYAN "It seems like the $file does not exist in your system"
+		mkdir -p $(dirname $original_location)
+		touch $original_location
+		pretty_print $GREEN "Copying over $file"
+		cp $dev_env_location $original_location
+		return
+	fi
+
+	has_diff=$(
+		cmp -s $original_location $dev_env_location
+		echo $?
+	)
+
+	if [[ $has_diff -eq 1 ]] && ! git diff --no-index $original_location $dev_env_location; then
 		if gum confirm "Do you want to replace your $file anyways"; then
 			backup $original_location
 			replace $dev_env_location $original_location
 		fi
 	else
-		echo "No difference in $file config"
+		echo "No difference in $file"
 		return 1
 	fi
 }
 
-echo "Checking brew formulae and casks"
+pretty_print $YELLOW "Checking brew formulae and casks"
 if ! (cd ./config/brew && brew bundle check --verbose --no-upgrade); then
 	(cd ./config/brew && brew bundle install --no-upgrade)
 fi
@@ -64,6 +85,8 @@ check_replace ./config/ghostty/config ~/.config/ghostty/config && announce "Relo
 
 check_replace ./config/lazygit/config.yml ~/.config/lazygit/config.yml || true
 
+pretty_print $YELLOW "Checking scripts"
 for FILE in "./scripts/bin"/*; do
-	echo "$FILE"
+	SCRIPT_NAME=$(basename $FILE)
+	check_replace $FILE ~/scripts/bin/$SCRIPT_NAME || true
 done
