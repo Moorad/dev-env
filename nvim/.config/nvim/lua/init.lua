32a1229872dd0d1953333a68c70bed02e17852f2
Moorad <moorad2003@gmail.com>
2026-03-12 14:38:07 +0300

    lazyvim

--- nvim/.config/nvim/lua/init.lua
+++ nvim/.config/nvim/lua/init.lua
@@ -1,23 +1,2 @@
-if vim.g.vscode then
-  -- Workaround for vscode-neovim UI desync (issue #2117)
-
-  -- 1. Redraw on CursorHold (idle for some time)
-  local redraw_fix = vim.api.nvim_create_augroup('VSCodeRedrawFix', { clear = true })
-  vim.api.nvim_create_autocmd('CursorHold', {
-    group = redraw_fix,
-    callback = function()
-      vim.cmd('silent! mode')  -- triggers a lightweight redraw
-    end,
-  })
-
-  -- 2. Redraw immediately after text changes (e.g., visual delete)
-  local redraw_group = vim.api.nvim_create_augroup('RedrawOnDelete', { clear = true })
-  vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
-    group = redraw_group,
-    callback = function()
-      if vim.fn.mode() == 'n' then
-        vim.cmd('silent! mode')  -- refresh UI after delete/insert
-      end
-    end,
-  })
-end
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
