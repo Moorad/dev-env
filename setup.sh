a88d5c2e85645db6ca65f0cbc1a68efc71a7ad8a
Moorad <moorad2003@gmail.com>
2026-08-15 21:25:13 +0300

    remove arch install, rename mac install to setup.sh and add stow all command

--- setup.sh
+++ setup.sh
@@ -9,3 +9,7 @@ if [ ! -d ~/.config/tmux/plugins/catppuccin ]; then
   mkdir -p ~/.config/tmux/plugins/catppuccin
   git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
 fi
+
+echo "Stowing all folders"
+
+stow */
